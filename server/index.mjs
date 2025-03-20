05f4686ccc2c331b217e720cb760c67009cb74d3
Viktoria Vonsovych <vikvons@gmail.com>
2025-03-20 23:47:07 +0100

    initial deployment

--- server/index.mjs
+++ server/index.mjs
@@ -7,6 +7,12 @@ app.use(cors())
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 
 
 // import { createClient } from '@supabase/supabase-js'
