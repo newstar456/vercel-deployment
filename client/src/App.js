202dc5ad921ff1765682bef0b221b6275010eaeb
Viktoria Vonsovych <vikvons@gmail.com>
2025-03-21 02:02:11 +0100

    initial deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,249 @@
-import logo from './logo.svg';
-import './App.css';
-  import axios from "axios"
+// import logo from './logo.svg';
+// import './App.css';
+//   import axios from "axios"
+// import ShopsList from "./features/shops/ShopsList";
+import MainPage from "./features/mainPage/MainPage";
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+// import ShopsListExtended from "./features/shops/ShopsListExtended";
+// import { ShopPage } from "./features/shops/ShopPage";
+// import { FinalPage } from "./features/finalPage/FinalPage";
+// import { History } from "./features/history/History";
+// import {CouponsList} from "./features/coupons/CouponsList";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Cart from './features/cart/Cart';
+import NotFoundPage from './features/NotFoundPage';
+import RootLayout from "./layout/RootLayout";
+
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout/>}>
+      <Route index element={<MainPage/>}/>
+      <Route path='cart' element={<Cart/>}/>
+      <Route path='*' element={<NotFoundPage/>}/>
+    </Route>
+  )
+);
+
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    common:{
+      black: '#000000',
+      white: '#FFFFFF',
+    },
+    background: {//beige
+      default: '#FBF2EA',
+      paper: '#ECB686',
+      paperLight:'#FEF9F6',
+    },
+    text: {//brown, black
+      primary: '#493E33',
+      secondary: '#A78F74',
+      disabled: '#A7A098',
+      hint: '#A4A09D',
+    },
+    primary: {//yellow
+      main: '#EBC080',
+      light: '#F7D7A8',
+      dark: '#CF8923',
+    },
+    secondary: {//green
+      main: '#8C9E94',
+      light: '#AAC0B4',
+      dark: '#73827A',
+    },
+    error: {//red
+      main: '#F20000',
+      light: '#F25757',
+      dark: '#A23A3A',
+    },
+    success: {//green
+      main: '#517250',
+      light: '#6F9C6E',
+      dark: '#405A40',
+    },
+  },
+  components: {
+    MuiToolbar: {
+      styleOverrides: {
+        root: {
+          boxShadow: '0px 1px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 1px 0px rgba(0,0,0,0.12)',
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.variant === 'standard' && {
+            '& label.Mui-focused': {
+              color: '#A78F74',
+            },
+            '& .MuiInput-underline:after': {
+              borderBottomColor: '#74726E',
+            },
+            }),
+        }),
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.variant === 'contained' && {
+            boxShadow: '0px 1px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 1px 0px rgba(0,0,0,0.12)',
+            backgroundColor: '#8C9E94',
+            color: '#FFFFFF',
+            '&:hover': {
+              backgroundColor: '#73827A',
+              boxShadow: '0px 0px 0px',
+            },
+            '& label.Mui-focused': {
+              color: '#A78F74',
+            },
+            '& .MuiInput-underline:after': {
+              borderBottomColor: '#74726E',
+            },
+            }),
+            ...(ownerState.variant === 'outlined' && {
+              border: `1px solid #73827A`,
+              color: '#73827A',
+              '&:hover': {
+                boxShadow: '1px 1px 1px #73827A',
+                border: `1px solid #73827A`,
+              },
+            }),
+        }),
+      },
+    },
+  },
+});
+
+const darkTheme = createTheme({
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 600,
+      laptop: 900,
+      desktop: 1200
+    }
+  },
+  spacing: [0, 4, 8, 16, 32, 64],
+  palette: {
+    mode: 'dark',
+    common:{
+      black: '#000000',
+      white: '#FFFFFF',
+    },
+    background: {
+      default: '#303841',
+      paper: '#F6C90E',
+      paperLight:'#EEEEEE',
+    },
+    text: {
+      primary: '#222831',
+      secondary: '#EEEEEE',
+      disabled: '#EEEEEE',
+      hint: '#EEEEEE',
+    },
+    primary: {
+      main: '#303841',
+      light: '#3A4750',
+      dark: '#000000',
+    },
+    secondary: {
+      main: '#F6C90E',
+      light: '#AAC0B4',
+      dark: '#DDE6ED',
+    },
+    error: {
+      main: '#F20000',
+      light: '#F25757',
+      dark: '#A23A3A',
+    },
+    success: {
+      main: '#517250',
+      light: '#6F9C6E',
+      dark: '#405A40',
+    },
+  },
+  components: {
+    MuiToolbar: {
+      styleOverrides: {
+        root: {
+          boxShadow: '0px 1px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 1px 0px rgba(0,0,0,0.12)',
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.variant === 'standard' && {
+            '& label.Mui-focused': {
+              color: '#EEEEEE',
+            },
+            '& .MuiInput-underline:after': {
+              borderBottomColor: '#EEEEEE',
+            },
+            }),
+        }),
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.variant === 'contained' && {
+            boxShadow: '0px 1px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 1px 0px rgba(0,0,0,0.12)',
+            backgroundColor: '#F6C90E',
+            color: '#393E46',
+            '&:hover': {
+              backgroundColor: '#F6C90E',
+              boxShadow: '1px 1px 1px #393E46',
+            },
+            '& label.Mui-focused': {
+              color: '#A78F74',
+            },
+            '& .MuiInput-underline:after': {
+              borderBottomColor: '#74726E',
+            },
+            }),
+            ...(ownerState.variant === 'outlined' && {
+              border: `1px solid #EEEEEE`,
+              color: '#EEEEEE',
+              '&:hover': {
+                outline: `1px solid #EEEEEE`,
+              },
+            }),
+        }),
+      },
+    },
+  },
+});
 
 function App() {
 
-    const fetchData = async () => {
-  try {
-    // await axios.get("http://localhost:3000/home")
-    await axios.get("https://vercel-deployment-five-xi.vercel.app/shops")
-      .then((response) => {
-        console.log(response.data);
+//     const fetchData = async () => {
+//   try {
+//     // await axios.get("http://localhost:3000/home")
+//     await axios.get("https://vercel-deployment-five-xi.vercel.app/shops")
+//       .then((response) => {
+//         console.log(response.data);
 
-      });
-  } catch (error) {
-    // Handle error
-    console.error(error);
-  }
-};
+//       });
+//   } catch (error) {
+//     // Handle error
+//     console.error(error);
+//   }
+// };
 
-fetchData();
+// fetchData();
+
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+
+      <RouterProvider router={router}/>
+    </ThemeProvider>
+
   );
 }
 
--- client/src/features/mainPage/MainPage.js
+++ client/src/features/mainPage/MainPage.js
@@ -3,22 +3,32 @@ import ShopsList from "../shops/ShopsList"
 import { createContext, useState} from "react"
 import Stack from '@mui/material/Stack'
 
+import React from 'react'
 
-const ShopChosen = createContext('hello');
-
-export const MainPage = () => {
-
-  const [shop, setShop] = useState('default');
-  const [byPrice, setByPrice] = useState(false);
-
+const MainPage = () => {
   return (
-    <ShopChosen.Provider value={shop}>
-      <Stack direction={{xs:'column', md:'row'}} spacing={{ xs:1, sm:2 }} >
-        <ShopsList setShop={setShop}/>
-        {/* <MedsList shop={shop} byPrice={byPrice} /> */}
-      </Stack>
-     </ShopChosen.Provider>
+    <div>MainPage</div>
   )
 }
 
-export {ShopChosen}
+export default MainPage
+
+
+// const ShopChosen = createContext('hello');
+
+// export const MainPage = () => {
+
+//   const [shop, setShop] = useState('default');
+//   const [byPrice, setByPrice] = useState(false);
+
+//   return (
+//     <ShopChosen.Provider value={shop}>
+//       <Stack direction={{xs:'column', md:'row'}} spacing={{ xs:1, sm:2 }} >
+//         <ShopsList setShop={setShop}/>
+//         <MedsList shop={shop} byPrice={byPrice} />
+//       </Stack>
+//      </ShopChosen.Provider>
+//   )
+// }
+
+// export {ShopChosen}
